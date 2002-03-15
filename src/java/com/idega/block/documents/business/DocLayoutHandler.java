5b160b68246f366f7585d944f19f85614f2369c9
Gudmundur Agust Saemundsson <gummi@idega.com>
2002-03-15 12:09:32 +0000

    no message

---
+++ src/java/com/idega/block/documents/business/DocLayoutHandler.java
@@ -0,0 +1,56 @@
+/*
+ * $Id: DocLayoutHandler.java,v 1.1 2002/03/15 12:09:32 gummi Exp $
+ *
+ * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
+package com.idega.block.documents.business;
+
+import java.util.List;
+import com.idega.block.documents.presentation.Doc;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.text.Text;
+import com.idega.builder.handler.PropertyHandler;
+
+/**
+ * @author <a href="tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.0
+ */
+public class DocLayoutHandler implements PropertyHandler {
+  /**
+   *
+   */
+  public DocLayoutHandler() {
+  }
+
+  /**
+   *
+   */
+  public List getDefaultHandlerTypes() {
+    return(null);
+  }
+
+  /**
+   *
+   */
+  public PresentationObject getHandlerObject(String name, String value, IWContext iwc) {
+    DropdownMenu menu = new DropdownMenu(name);
+    menu.addMenuElement("","Select:");
+    menu.addMenuElement(Doc.BOX_VIEW,"Doc view");
+    menu.addMenuElement(Doc.CATEGORY_VIEW,"Category view");
+    menu.addMenuElement(Doc.COLLECTION_VIEW,"Collection view");
+    menu.setSelectedElement(value);
+    return(menu);
+  }
+
+  /**
+   *
+   */
+  public void onUpdate(String values[], IWContext iwc) {
+  }
+}
